b582447f1914b0185e07fa465202037d6b10b134
John Roughley <jroughley@softwareinstitute.com>
2022-09-09 11:11:10 +0100

    changed main() argument arvgv --> argv

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include"../inc/adder.hpp"
 
-int main(int argc, char** arvgv)
+int main(int argc, char** argv)
 {
     std::cout << "From main: Adder(3,4) = " << Adder(3,4) << ".\n";
     return 0;
